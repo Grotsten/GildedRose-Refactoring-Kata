a1d5352680e0c19ef5344aebc4a7fe0ba3795786
Elliot Barnes <Elliotbarnes123@gmail.com>
2020-07-07 18:33:57 +0100

    Organised the code into a function for processing each type

--- Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
+++ Kotlin/src/main/kotlin/com/gildedrose/GildedRose.kt
@@ -1,56 +1,90 @@
 package com.gildedrose
 
 class GildedRose(var items: Array<Item>) {
+    fun agedBrieProcessor(item: Item){
+        if (item.quality < 50) {
+            if (item.sellIn > 0) {
+                item.quality += 1
+            } else{
+                item.quality += 2
+            }
+        }
+        item.sellIn -= 1
+    }
+    fun normalItemProcessor(item: Item){
+        if (item.sellIn<1) {
+            item.quality -= 2
+        } else {
+            item.quality -= 1
+        }
+        if (item.quality < 0){
+            item.quality = 0
+        } else if (item.quality > 50) {
+            item.quality = 50
+        }
+        item.sellIn -= 1
+    }
+    fun sulfurasProcessor(item: Item){
+        item.quality = 80
+    }
+    fun concertProcessor(item: Item){
+        if (item.quality < 50) {
+            item.quality = item.quality + 1
+
+            if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1
+                    }
+                }
+            }
+        }
+
+        if (item.sellIn < 1) {
+            item.quality = 0
+        }
+        item.sellIn -= 1
+    }
+
+    fun checkConjured(item: Item): Boolean {
+        return item.name.startsWith("Conjured")
+    }
+
+    fun conjuredProcessor(item: Item) {
+        item.quality -= 2
+        if (item.sellIn < 1) {
+            item.quality -= 2
+        }
+        item.sellIn -= 1
+        if (item.quality < 0){
+            item.quality = 0
+        } else if (item.quality > 50){
+            item.quality = 50
+        }
+     }
+    fun processItem(item: Item) {
+        if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
+            concertProcessor(item)
+        } else if (item.name == "Sulfuras, Hand of Ragnaros"){
+            sulfurasProcessor(item)
+        } else if (item.name == "Aged Brie") {
+            agedBrieProcessor(item)
+        } else if (checkConjured(item)){
+            conjuredProcessor(item)
+        } else {
+            normalItemProcessor(item)
+        }
+    }
 
     fun updateQuality() {
-        for (i in items.indices) {
-            if (items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                if (items[i].quality > 0) {
-                    if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                        items[i].quality = items[i].quality - 1
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1
-
-                    if (items[i].name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                items[i].sellIn = items[i].sellIn - 1
-            }
-
-            if (items[i].sellIn < 0) {
-                if (items[i].name != "Aged Brie") {
-                    if (items[i].name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if (items[i].quality > 0) {
-                            if (items[i].name != "Sulfuras, Hand of Ragnaros") {
-                                items[i].quality = items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1
-                    }
-                }
-            }
+        for (item in items) {
+            processItem(item)
         }
     }
 
--- Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
+++ Kotlin/src/test/kotlin/com/gildedrose/GildedRoseTest.kt
@@ -9,11 +9,145 @@ class GildedRoseTest {
         val items = arrayOf<Item>(Item("foo", 0, 0))
         val app = GildedRose(items)
         app.updateQuality()
-        assertEquals("fixme", app.items[0].name)
-
+        assertEquals("foo", app.items[0].name)
     }
 
+    //test the sulfuras quality doesn't change
+    @Test fun sulfurasQuality() {
+        val items = arrayOf<Item>(Item("Sulfuras, Hand of Ragnaros",5,80),
+                Item("Sulfuras, Hand of Ragnaros",1,80),
+                Item("Sulfuras, Hand of Ragnaros",-1,80))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(80, app.items[0].quality)
+        assertEquals(80,app.items[1].quality)
+        assertEquals(80,app.items[2].quality)
+    }
 
+    //Test the sulfuras sellin doesn't decrease
+    @Test fun sulfurasSellIn() {
+        val items = arrayOf<Item>(Item("Sulfuras, Hand of Ragnaros",5,80),
+                Item("Sulfuras, Hand of Ragnaros",-1,80))
+        val app = GildedRose(items)
+        assertEquals(5, app.items[0].sellIn)
+        assertEquals(-1, app.items[1].sellIn)
+    }
+
+    //Test the concert ticket behaviour 10+ days
+    @Test fun concert10plusDays() {
+        val items = arrayOf<Item>( Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+                Item("Backstage passes to a TAFKAL80ETC concert", 10, 30))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(21, app.items[0].quality)
+        assertEquals(32, app.items[1].quality)
+    }
+
+    //Test the concert ticket behaviour 5-10 days
+    @Test fun concert5plusDays() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 5, 45),
+                Item("Backstage passes to a TAFKAL80ETC concert", 7,49))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(48, app.items[0].quality)
+        assertEquals(50, app.items[1].quality)
+    }
+
+    //Test the concert ticket behaviour 1-5 days
+    @Test fun concert1Day() {
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 1,34),
+                Item("Backstage passes to a TAFKAL80ETC concert",3,50),
+                Item("Backstage passes to a TAFKAL80ETC concert", 2,45))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(37, app.items[0].quality)
+        assertEquals(50, app.items[1].quality)
+        assertEquals(48, app.items[2].quality)
+    }
+
+    //Test the concert tickets after the concert happens
+    @Test fun concert0Day(){
+        val items = arrayOf<Item>(Item("Backstage passes to a TAFKAL80ETC concert", 0,48),
+                Item("Backstage passes to a TAFKAL80ETC concert",-4,4))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(0, app.items[0].quality)
+        assertEquals(0, app.items[1].quality)
+    }
+
+    //Test Brie behaviour when in date
+    @Test fun brieInDate() {
+        val items = arrayOf<Item>(Item("Aged Brie",2,2),
+                Item("Aged Brie", 3,50))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(3, app.items[0].quality)
+        assertEquals(50,app.items[1].quality)
+    }
+
+    //Tests the behaviour when Brie is out of date
+    @Test fun brieOutOfDate() {
+        val items = arrayOf<Item>(Item("Aged Brie", 0, 5),
+                Item("Aged Brie", -4,3),
+                Item("Aged Brie", -4, 50))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(7, app.items[0].quality)
+        assertEquals(5, app.items[1].quality)
+        assertEquals(50, app.items[2].quality)
+    }
+    //Tests the behaviour with normal items in date
+    @Test fun normalInDate() {
+        val items = arrayOf<Item>(Item("Front stage pass", 5,4),
+                Item("Apple", 7,50),
+                Item("Glass", 4,0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(3, app.items[0].quality)
+        assertEquals(49, app.items[1].quality)
+        assertEquals(0, app.items[2].quality)
+    }
+
+    //Tests the behaviour with normal items out of date
+    @Test fun normalOutOfDate() {
+        val items = arrayOf<Item>(Item("Pen",0,3),
+                Item("Shoe", -2, 50),
+                Item("Bowl", -5,0),
+                Item("Shirt", -3,1))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(1, app.items[0].quality)
+        assertEquals(48, app.items[1].quality)
+        assertEquals(0, app.items[2].quality)
+        assertEquals(0, app.items[3].quality)
+    }
+
+    //Tests the conjured item in date
+    @Test fun conjuredInDate(){
+        val items = arrayOf<Item>(Item("Conjured Pen", 5, 5),
+                Item("Conjured Shoe", 5,1),
+                Item("Conjured Bowl", 3, 0),
+                Item("Conjured Shirt", 4, 50))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(3, app.items[0].quality)
+        assertEquals(0, app.items[1].quality)
+        assertEquals(0, app.items[2].quality)
+        assertEquals(48, app.items[3].quality)
+    }
+
+    //Test an out of date conjured item
+    @Test fun conjuredOutOfDate() {
+        val items = arrayOf<Item>(Item("Conjured Pen", 0, 5),
+                Item("Conjured Shoe", -1, 3),
+                Item("Conjured Bowl", -3, 0))
+        val app = GildedRose(items)
+        app.updateQuality()
+        assertEquals(1, app.items[0].quality)
+        assertEquals(0, app.items[1].quality)
+        assertEquals(0, app.items[2].quality)
+
+    }
 }
 
 
